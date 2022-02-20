3f61cc59e6a823b3af86f9e62ba31f4a846c7d1f
Svetoslav Savov <speednet@mail.bg>
2022-02-20 19:49:42 +0200

    new

---
+++ Inheritance/Inheritance Lab/1.SingleInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.SingleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
