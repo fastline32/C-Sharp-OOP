6a42202b65c0125b9a4ff281ef3fa4caaeb8c18f
Svetoslav Savov <speednet@mail.bg>
2022-02-20 22:43:09 +0200

    new

---
+++ Inheritance/Inheritance Lab/4.RandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string ReturnRandomIndex()
+        {
+            int index = random.Next(0,Count);
+            return this[index];
+        }
+
+        public void RemoveRandomElement()
+        {
+            int index = random.Next(0, Count);
+            RemoveAt(index);
+        }
+    }
+}
