3e3438696659e5e348d4c821166616ccaa6aa4e3
Svetoslav Savov <speednet@mail.bg>
2022-02-24 20:32:08 +0200

    new

---
+++ Encapsulation/Encapsulation - Lab/ValidationOfData/Person.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get 
+            { return firstName; 
+            }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName 
+        { get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }  
+        }
+        public int Age 
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                    return;
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary 
+        {
+            get 
+            {
+
+                return this.salary;
+            }
+            private set 
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            } 
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += (Salary * percentage) / 100;
+            }
+            else
+            {
+                Salary += (Salary * percentage) / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
