5a8b0698074e98872178379b40a8de674cce44da
Svetoslav Savov <speednet@mail.bg>
2022-02-25 09:11:26 +0200

    new

---
+++ Encapsulation/Encapsulation - Lab/FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("Beasts");
+            int lines = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()!.Split();
+                var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    decimal.Parse(cmdArgs[3]));
+
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation - Lab/FirstAndReserveTeam/Team.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name { get => name; set => name = value; }
+        public IReadOnlyCollection<Person> FirstTeam { get => firstTeam.AsReadOnly(); }
+        public IReadOnlyCollection<Person> ReserveTeam { get => reserveTeam.AsReadOnly(); }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
